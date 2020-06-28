e23e218d85f6e45a7b6edb415ac726390d01becb
CHUNHAO WENG <chunhaoweng@Jimmys-MacBook-Air.local>
2020-06-28 21:12:44 +0800

    hw2 done

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,9 @@
 function capitalize(str) {
-  
+  if (str.charCodeAt(0) >= 97 && str.charCodeAt(0) <= 122) {
+    return String.fromCharCode(str.charCodeAt(0) - 32) + str.slice(1)
+  } else {
+    return str
+  }
 }
 
 console.log(capitalize('hello'));
