5f2ae0c812ee116f0f27e168c8f8bf4df9e67ed4
CHUNHAO WENG <chunhaoweng@Jimmys-MacBook-Air.local>
2020-07-05 23:05:58 +0800

    hw2 eslint

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,32 +1,58 @@
-var readline = require('readline');
-const { type } = require('os');
+const readline = require('readline');
 
-var rl = readline.createInterface({
-  input: process.stdin
+const lines = [];
+const rl = readline.createInterface({
+  input: process.stdin,
 });
 
-var lines = []
+rl.on('line', line => lines.push(line));
 
-rl.on('line', function (line) {
-  lines.push(line)
-});
-
-rl.on('close', function (line) {
-  solve(lines)
-});
-
-function solve(lines) {
-  var tmp = lines[0].split(' ')
+function solve(object) {
+  const tmp = object[0].split(' ');
   for (let i = Number(tmp[0]); i <= Number(tmp[1]); i += 1) {
-    let total = 0
-    let n = i.toString().length // 長度就是 n次方
-    let arr = i.toString().split('')
-    for (j = 0; j < n; j += 1) { //每個平方加總
-      total += (Number(arr[j]) ** n)
+    let total = 0;
+    const n = i.toString().length; // 長度就是 n次方
+    const arr = i.toString().split('');
+    for (let j = 0; j < n; j += 1) { // 每個平方加總
+      total += (Number(arr[j]) ** n);
     }
     if (i === total) {
-      console.log(i)
+      console.log(i);
     }
   }
-
 }
+
+rl.on('close', () => solve(lines));
+
+// var readline = require('readline');
+// const { type } = require('os');
+
+// var rl = readline.createInterface({
+//   input: process.stdin
+// });
+
+// var lines = []
+
+// rl.on('line', function (line) {
+//   lines.push(line)
+// });
+
+// rl.on('close', function (line) {
+//   solve(lines)
+// });
+
+// function solve(lines) {
+//   var tmp = lines[0].split(' ')
+//   for (let i = Number(tmp[0]); i <= Number(tmp[1]); i += 1) {
+//     let total = 0
+//     let n = i.toString().length // 長度就是 n次方
+//     let arr = i.toString().split('')
+//     for (j = 0; j < n; j += 1) { //每個平方加總
+//       total += (Number(arr[j]) ** n)
+//     }
+//     if (i === total) {
+//       console.log(i)
+//     }
+//   }
+
+// }
