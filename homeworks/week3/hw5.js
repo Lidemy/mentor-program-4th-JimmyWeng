827a64a5df15baeb6f3da9546678f27b2c28dbee
CHUNHAO WENG <chunhaoweng@Jimmys-MacBook-Air.local>
2020-07-11 16:22:36 +0800

    hw5 done

--- homeworks/week3/hw5.js
+++ homeworks/week3/hw5.js
@@ -1,36 +1,63 @@
-var readline = require('readline');
-const { type } = require('os');
+/* eslint-disable indent */
+const readline = require('readline');
 
-var rl = readline.createInterface({
-  input: process.stdin
+const rl = readline.createInterface({
+    input: process.stdin,
 });
 
-var lines = []
+const lines = [];
 
-rl.on('line', function (line) {
-  lines.push(line)
+rl.on('line', (line) => {
+    lines.push(line);
 });
 
-rl.on('close', function (line) {
-  solve(lines)
-});
+const firstStrBigger = (strA, strB) => {
+    // 先不管字串前面可能有零（過不去再說）
+    if (strA.length === strB.length) { // If equal string length, check from left to right
+        for (let i = 0; i < strA.length; i += 1) {
+            // if (Number(strA[i]) === Number(strB[i])) {
+            //     continue;
+            // } else if (Number(strA[i]) > Number(strB[i])) {
+            //     return true;
+            // } else {
+            //     return false;
+            // }
+            if (Number(strA[i]) !== Number(strB[i])) {
+                if (Number(strA[i]) > Number(strB[i])) {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+    if (strA.length > strB.length) { // if not equal string length
+        return true;
+    }
+    return false;
+};
 
-const solve = lines => {
-  var rows = lines[0]
-  for (let i = 1; i <= rows; i += 1) {
-    // console.log(i)
-    let tmp = lines[i].split(' ')
-    let A = tmp[0]
-    let B = tmp[1]
-    let K = tmp[2]
-    // console.log('yay')
-    // console.log('A, B, K = ' + A + ', ' + B + ', ' + K)
-    // console.log('typeof A : ' + typeof A)
-    // console.log('typeof B : ' + typeof B)
-    // console.log('typeof K : ' + typeof K)
-    console.log(
-      (A === B) ? 'DRAW' : ((Number(K) === 1) ? (A > B ? 'A' : 'B') : (A > B ? 'B' : 'A'))
-    )
-  }
+function largerOrSmaller(num1, num2, method) {
+    if (num1 === num2) {
+        return 'DRAW'; // 一樣大
+    }
+
+    if (method === '1') { // 比大
+        // console.log('method 1')
+        return firstStrBigger(num1, num2) ? 'A' : 'B';
+    }
+    // 比小
+    // console.log('method 2')
+    return firstStrBigger(num1, num2) ? 'B' : 'A';
 }
 
+const solve = (input) => {
+    const tmp = input[0];
+    for (let i = 1; i <= tmp; i += 1) {
+        const [a, b, k] = input[i].split(' ');
+        console.log(largerOrSmaller(a, b, k));
+    }
+};
+
+rl.on('close', () => {
+    solve(lines);
+});
