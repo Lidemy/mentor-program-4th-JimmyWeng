f8a61e5f79c88e4db40f7f41719a7668eb65387e
CHUNHAO WENG <chunhaoweng@Jimmys-MacBook-Air.local>
2020-07-13 10:36:42 +0800

    save

---
+++ homeworks/week4/hw1.js
@@ -0,0 +1,12 @@
+// const request = require('request');
+
+// request('https://lidemy-book-store.herokuapp.com/books?_limit=10', function (error, response, body) {
+//   // console.error('error:', error);
+//   // console.log('statusCode:', response && response.statusCode);
+//   // console.log('head:', response.headers);
+//   console.log('body:', body);
+//   // console.log('Original format，JSON string — — — — — ');
+//   // console.log(body);
+//   // console.log('Parse to JavaScript Object — — — — — — — — — — ');
+//   // console.log(JSON.parse(body));
+// });
